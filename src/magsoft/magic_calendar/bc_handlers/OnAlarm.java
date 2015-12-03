e9fcc1fbd90a04056da08a1f0fffaa0ccb901485
Nurul Huda <hudasanca@gmail.com>
2015-12-04 06:20:02 +0700

    alarm detection for schedules

--- src/magsoft/magic_calendar/bc_handlers/OnAlarm.java
+++ src/magsoft/magic_calendar/bc_handlers/OnAlarm.java
@@ -5,10 +5,12 @@ import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.database.Cursor;
 import android.support.v4.app.NotificationCompat;
 import android.support.v4.app.TaskStackBuilder;
 import magsoft.magic_calendar.MainActivity;
 import magsoft.magic_calendar.R;
+import magsoft.magic_calendar.db.JadwalTable;
 
 public class OnAlarm extends BroadcastReceiver{
 
@@ -17,11 +19,20 @@ public class OnAlarm extends BroadcastReceiver{
 		/**
 		 * Building the notification
 		 * */
+		JadwalTable jadwal = new JadwalTable(context);
+		jadwal.open();
+		Cursor c = jadwal.getToday();
 		
 		NotificationCompat.Builder notifBuilder = new NotificationCompat.Builder(context);
 		notifBuilder.setSmallIcon(R.drawable.ic_launcher);
 		notifBuilder.setContentTitle("Magic Calendar");
-		notifBuilder.setContentText("Hari ini adalah hari libur! :)");
+		
+		if (c.getCount() > 0){
+			notifBuilder.setContentText("Hari ini ada "+c.getCount()+" agenda");
+		}
+		else {
+			notifBuilder.setContentText("Semoga hari ini berjalan lancar! :)");
+		}
 		
 		Intent i = new Intent(context, MainActivity.class);
 		TaskStackBuilder stackBuilder = TaskStackBuilder.create(context);
@@ -34,6 +45,8 @@ public class OnAlarm extends BroadcastReceiver{
 		
 		NotificationManager notifManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
 		notifManager.notify(0, notifBuilder.build());
+		
+		jadwal.close();
 	}
 
 }
