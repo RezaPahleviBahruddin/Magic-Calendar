010135aafbc4ff89609696b540522a1f12149e25
Nurul Huda <hudasanca@gmail.com>
2015-12-04 19:53:30 +0700

    improving the list of data by grouping it

--- src/magsoft/magic_calendar/ListReminderActivity.java
+++ src/magsoft/magic_calendar/ListReminderActivity.java
@@ -1,19 +1,38 @@
 package magsoft.magic_calendar;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Locale;
+
 import android.app.Activity;
 import android.content.Context;
 import android.database.Cursor;
 import android.os.Bundle;
+import android.text.format.DateFormat;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AbsListView;
+import android.widget.AbsListView.OnScrollListener;
 import android.widget.BaseAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 import magsoft.magic_calendar.db.JadwalTable;
 
-public class ListReminderActivity extends Activity{
+public class ListReminderActivity extends Activity implements OnScrollListener{
+	private int firstVisibleItem = 0;
+	private Cursor c;
+	private TextView txtFixed;
+	private int month = 0;
+	private SimpleDateFormat dateFormat;
+	private Calendar cal;
+	private final String[] monthInString= {"Januari", "Februari", "Maret", "April", "Mei", "Juni", "Juli", "Agustus", "September", "Oktober",
+			"November", "Desember"};
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState){
@@ -23,12 +42,28 @@ public class ListReminderActivity extends Activity{
 		
 		ListView listView = (ListView) findViewById(R.id.listSchedule);
 		
+		txtFixed = (TextView) findViewById(R.id.txtFixed);
+		
 		JadwalTable jadwal = new JadwalTable(getApplicationContext());
 		jadwal.open();
-		Cursor c = jadwal.getAll();
+		c = jadwal.getAll();
 //		jadwal.close();
 		CustomAdapter ca = new CustomAdapter(c);
+		dateFormat = new SimpleDateFormat("yyyy-MM-dd", Locale.getDefault());
+		cal = Calendar.getInstance();
+		c.moveToFirst();
+		
+		try {
+			cal.setTime(dateFormat.parse(c.getString(3)));
+		} catch (ParseException e) {
+			e.printStackTrace();
+		}
+		
+		txtFixed.setText(monthInString[cal.get(Calendar.MONTH)]);
+		
 		listView.setAdapter(ca);
+		
+		listView.setOnScrollListener(this);
 	}
 	
 	class CustomAdapter extends BaseAdapter{
@@ -70,6 +105,30 @@ public class ListReminderActivity extends Activity{
 			tv2.setText(c.getString(3));
 			return v;
 		}
-		
+	}
+
+	@Override
+	public void onScrollStateChanged(AbsListView view, int scrollState) {
+	}
+
+	@Override
+	public void onScroll(AbsListView view, int firstVisibleItem, int visibleItemCount, int totalItemCount) {
+		if (this.firstVisibleItem != firstVisibleItem){
+			Log.d("Magsoft", "firstVisibleItem -> "+firstVisibleItem+"; visibleItemCount -> "+visibleItemCount+"; totalItemCount -> "+totalItemCount);
+			this.firstVisibleItem = firstVisibleItem;
+			c.moveToPosition(firstVisibleItem);
+			
+			try {
+				cal.setTime(dateFormat.parse(c.getString(3)));
+			} catch (ParseException e) {
+				e.printStackTrace();
+			}
+
+			if (month != cal.get(Calendar.MONTH)) {
+				Log.d("Magsoft", "month -> "+cal.get(Calendar.MONTH));
+				month = cal.get(Calendar.MONTH);
+				txtFixed.setText(monthInString[month]);
+			}
+		}
 	}
 }
